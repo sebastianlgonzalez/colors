9b81c9981aea5f7f2e020af511f6ac046a64870e
sebastianlgonzalez <sebastianlgonzalez01@gmail.com>
2024-03-18 18:33:02 -0700

    input form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,45 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { IoIosReturnLeft } from "react-icons/io"
+
 export default function App() {
   const [inputValue, setInputValue] = useState("")
+  const buttonRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputValue)
+    buttonRef.current.setAttribute("data-active", "true")
+    setTimeout(() => {
+      buttonRef.current.setAttribute("data-active", "false")
+    }, 100)
+  }
+
   return (
     <main className="flex h-screen w-full items-center">
-      <div className="mx-auto flex h-fit w-[500px]">
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto flex h-fit w-[500px] rounded-full shadow-md"
+      >
         <input
-          className="h-10 flex-grow rounded-l-full border-2 pl-3 focus:z-10 focus:border-slate-500 focus:outline-none"
+          className="h-10 flex-grow rounded-l-full border-2 border-neutral-300 pl-3 text-neutral-700 focus:z-10 focus:border-neutral-400 focus:outline-none"
           placeholder="Enter text to generate colors..."
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-        ></input>
+        />
         <button
-          onClick={() => {
-            console.log(inputValue)
-          }}
-          className="ml-[-2px] flex h-10 w-14 items-center rounded-r-full border-2 focus:border-slate-500 focus:outline-none"
+          ref={buttonRef}
+          type="submit"
+          className="ml-[-2px] flex h-10 w-14 items-center rounded-r-full border-2 border-neutral-300 hover:bg-neutral-100 focus-visible:border-neutral-400 focus-visible:outline-none data-[active=true]:bg-neutral-200"
+          data-active="false"
         >
           <IoIosReturnLeft
-            color="black"
-            className="ml-[7px]"
-            size={30}
-            strokeWidth={10}
+            color="#404040"
+            className="ml-[9px]"
+            size={27}
+            strokeWidth={2}
           />
         </button>
-      </div>
+      </form>
     </main>
   )
 }
